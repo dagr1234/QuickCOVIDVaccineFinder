803965299b5e1002cbaa165ffbe1044f70121028
dagr1234 <david.grossman1234@gmail.com>
2021-04-22 21:32:55 -0400

    Initial Commit

---
+++ QuickCOVIDVaccineFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickCOVIDVaccineFinder
+//
+//  Created by David Grossman on 4/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickCOVIDVaccineFinder/QuickCOVIDVaccineFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCOVIDVaccineFinderApp.swift
+//  QuickCOVIDVaccineFinder
+//
+//  Created by David Grossman on 4/22/21.
+//
+
+import SwiftUI
+
+@main
+struct QuickCOVIDVaccineFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
