d5038c6634a4793fb667b9481c46da99ca1814ad
dagr1234 <david.grossman1234@gmail.com>
2021-04-22 21:46:39 -0400

    v0.1

--- QuickCOVIDVaccineFinder/ContentView.swift
+++ QuickCOVIDVaccineFinder/ContentView.swift
@@ -7,10 +7,40 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    @State var results = [VaccineEntry]()
+    
+    
+    func loadData() {
+            guard let url = URL(string: "https://www.vaccinespotter.org/api/v0/states/VA.json") else {
+                print("Invalid URL")
+                return
+            }
+            let request = URLRequest(url: url)
+
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let data = data {
+                    if let response = try? JSONDecoder().decode([VaccineEntry].self, from: data) {
+                        DispatchQueue.main.async {
+                            self.results = response
+                        }
+                        return
+                    }
+                }
+            }.resume()
+        }
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(results, id: \.id) { item in
+                    VStack(alignment: .leading) {
+                        Text(item.title)
+                    }
+                }.onAppear(perform: loadData)
     }
 }
 
---
+++ QuickCOVIDVaccineFinder/VaccineEntry.swift
@@ -0,0 +1,13 @@
+//
+//  VaccineEntry.swift
+//  QuickCOVIDVaccineFinder
+//
+//  Created by David Grossman on 4/22/21.
+//
+
+import Foundation
+
+struct VaccineEntry: Codable  {
+    let id:    Int
+    let title: String
+}
