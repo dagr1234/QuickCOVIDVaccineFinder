75694b9e3bd31bae0154e4a43f83ab6e1d40efac
dagr1234 <david.grossman1234@gmail.com>
2021-04-25 00:10:19 -0400

    got parsing to work at long last

--- QuickCOVIDVaccineFinder/ContentView.swift
+++ QuickCOVIDVaccineFinder/ContentView.swift
@@ -8,13 +8,15 @@
 import SwiftUI
 
 
-
 struct ContentView: View {
     
-    @State var results = [VaccineEntry]()
+    @State var results : [VaccineEntry] = []
     
     
     func loadData() {
+        
+            var result : VaccineEntry = VaccineEntry()
+            print("Starting load...")
             guard let url = URL(string: "https://www.vaccinespotter.org/api/v0/states/VA.json") else {
                 print("Invalid URL")
                 return
@@ -22,26 +24,72 @@ struct ContentView: View {
             let request = URLRequest(url: url)
 
             URLSession.shared.dataTask(with: request) { data, response, error in
-                if let data = data {
-                    if let response = try? JSONDecoder().decode([VaccineEntry].self, from: data) {
-                        DispatchQueue.main.async {
-                            self.results = response
+                guard let json = try? JSON(data : data!) else {
+                    return
+                }
+                for features in json["features"].arrayValue {
+                    print(features)
+                    for properties in features["properties"] {
+            
+                        if (properties.0 == "url") {
+                            result.url = properties.1.stringValue
                         }
-                        return
+                        
+                        if (properties.0 == "id") {
+                            result.id = properties.1.intValue
+                        }
+                        
+                        if (properties.0 == "provider") {
+                            result.provider = properties.1.stringValue
+                        }
+                        
+                        if (properties.0 == "city") {
+                            result.city = properties.1.stringValue
+                        }
+                        
+                        if (properties.0 == "state") {
+                            result.state = properties.1.stringValue
+                        }
+                        
+                        if (properties.0 == "postal_code") {
+                            result.zip_code = properties.1.stringValue
+                        }
+                        
+                        if (properties.0 == "address") {
+                            result.address = properties.1.stringValue
+                        }
+                        
+                        if (properties.0 == "appointments_available") {
+                            result.appointments_available = properties.1.boolValue
+                        }
+                        
+                        
                     }
+                    DispatchQueue.main.async {
+                        results.append(result)
+                    }
+                    
                 }
             }.resume()
+            
         }
     
     
     
     var body: some View {
-        List(results, id: \.id) { item in
-                    VStack(alignment: .leading) {
-                        Text(item.title)
-                    }
-                }.onAppear(perform: loadData)
+        
+        VStack {
+            Text("Results")
+            
+            List(results.identified(by: \.id)) { result in
+                Text(result.url)
+            }
+            
+        }.onAppear(perform: loadData)
+            
     }
+        
+
 }
 
 struct ContentView_Previews: PreviewProvider {
--- QuickCOVIDVaccineFinder/VaccineEntry.swift
+++ QuickCOVIDVaccineFinder/VaccineEntry.swift
@@ -7,7 +7,32 @@
 
 import Foundation
 
-struct VaccineEntry: Codable  {
-    let id:    Int
-    let title: String
+struct VaccineEntry : Decodable  {
+    var id:                   Int // Vaccine Spotter's own unique ID for this location.
+    var url      :            String
+    var provider :            String // A unique key representing the pharmacy or provider this location belongs to.
+    var city     :            String
+    var zip_code :            String
+    var address  :            String
+    var state    :            String
+    var name     :            String
+    var appointments_available: Bool  //value as to whether or not appointments are currently available at this location as of last check.
+    
+    init() {
+        self.id = 0
+        self.url = ""
+        self.provider = ""
+        self.appointments_available = false
+        self.city = ""
+        self.zip_code = ""
+        self.address = ""
+        self.name = ""
+        self.state = ""
+    }
 }
+
+struct VaccineEntries: Decodable {
+  var results: [VaccineEntry]
+}
+
+
